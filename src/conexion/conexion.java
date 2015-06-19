54cc4684c2bc326be18fae99c94069829363ee4b
enriqueramirez <enriqueramirez@192.168.15.30>
2015-06-19 09:13:29 -0500

    base de datos con errores

--- src/conexion/conexion.java
+++ src/conexion/conexion.java
@@ -5,79 +5,54 @@
  */
 package conexion;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import javax.swing.JOptionPane;
+import java.sql.*;
+    
 
 /**
  *
  * @author enriqueramirezgonzalez
  */
 public class conexion {
-    /**
-     * Creacion de la conección
-     */
-    Connection Db = null;
+    private static Connection conexion;
+    private static final String bd="db_gestionElectiva";
+    private static final String user="saul";
+    private static final String password="123456";
+    private static final String host="localhost";
+    private static final String server="jdbc:mysql://"+host+"/"+bd;
     
-    /**
-     * Estado de la respuesta
-     */
-    Statement DataRequest;
-    
-    /**
-     * Lo que regresa las sentencias de mysql
-     */
-    ResultSet Resultado;
-    /**
-     * Ruta de conecicon a base de datos
-     */
-    String url = "jdbc:mysql://127.0.0.1:3306/db_gestionElectiva";
-    
-    /**
-     * usuario de la base de datos
-     */
-    String usuario = "saul";
-    
-    /**
-     * Contraseña del usuario de la base de datos
-     */
-    String password = "123456";
-    
-    /**
-     * Método para crear la conección a base de datos
-     * @return 
-     */
-    public boolean Conexion(){
-        boolean a = false;
-        try{
+     public static void main(String[] args) {
+        try {
             Class.forName("com.mysql.jdbc.Driver");
-            Db = (Connection)DriverManager.getConnection(url, usuario, password);
-            a = true;
-        }catch(ClassNotFoundException error){
-            System.err.println("No se puede conectara a la base de datos" + error);
-        }catch(SQLException error){
-            JOptionPane.showMessageDialog(null, "Error en la contraseña");
+            conexion = DriverManager.getConnection(server,user,password);
+            System.out.println("Conexión a base de datos " + server + " ... OK");
+        } catch (ClassNotFoundException ex) {
+            System.out.println("Error cargando el Driver MySQL JDBC ... FAIL");
+            //Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            System.out.println("Imposible realizar conexion con " + server + " ... FAIL");
+            //Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
         }
-        return a;
-    }
-    
-    public int buscarUsuario(String user, String pass){
-        int b = 0;
         
-        try{
-            DataRequest = (Statement) Db.createStatement();
-            Resultado = DataRequest.executeQuery("Select * from usuarios where nickname = '" + user + "' and password = md5('" + pass + "');");
-            while(Resultado.next()){
-                b++;
+        //realizar consulta
+        try {
+            // Preparamos la consulta
+            Statement s = conexion.createStatement();
+            ResultSet rs = s.executeQuery ("select * from usuarios");
+            // Recorremos el resultado, mientras haya registros para leer, y escribimos el resultado en pantalla.
+            while (rs.next()){
+                System.out.println(rs);
             }
-        }catch(SQLException ex){
-            System.out.println(ex + "buscaUsuarios");
+        } catch (SQLException ex) {
+            System.out.println("Imposible realizar consulta ... FAIL");
+            //Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
-        return b;
+        //desconectar
+        try {
+            conexion.close();
+            System.out.println("Cerrar conexion con "+server+" ... OK");
+        } catch (SQLException ex) {
+            System.out.println("Imposible cerrar conexion ... FAIL");
+            //Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
